ea836702a161b6a88a0a93bcab31db2a4fe1d193
HADI AMAN <hadiaman2.0@gmail.com>
2021-06-05 19:00:56 +0530

    404, 403 CSRF verification errors are handled

--- home/views.py
+++ home/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import UrlModel
+from django.views.csrf import csrf_failure
+from validation.Validator import input_validation
 
 
 def home(request):
-    data = {'error': '', 'email_value': '', 'url_value': ''}
+    data = {'error': '', 'email_value': '', 'url_value': '', 'success_data': False}
     return render(request, 'index.html', data)
+
+
+def error_403_view(request, reason=""):
+    return render(request, '403.html', status=403)
+
+
+def error_404_view(request, exception):
+    return render(request, '404.html', status=404)
+
+
+def process(request):
+    try:
+        if request.method == 'GET':
+            return redirect('/')
+        if request.method == 'POST':
+            validation = input_validation(request.POST)
+            print(validation)
+            obj = redirect('/')
+            return obj
+    except UrlModel.DoesNotExist:
+        return HttpResponse('404')
