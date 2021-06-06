e1a8e1cce42ebf73a2e3da78e809eeda00429795
HADI AMAN <hadiaman2.0@gmail.com>
2021-06-06 22:30:49 +0530

    Debugged and set up cookies for error response

--- home/views.py
+++ home/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .models import UrlModel
-from django.views.csrf import csrf_failure
 from validation.Validator import input_validation
 
 
 def home(request):
-    data = {'error': '', 'email_value': '', 'url_value': '', 'success_data': False}
-    return render(request, 'index.html', data)
+    data = {'error': request.COOKIES['error'] if 'error' in request.COOKIES else False, 'email_value': '', 'url_value': ''}
+    success_data = False
+    if 'success_data' in request.COOKIES:
+        data['success_data'] = request.COOKIES['success_data']
+        success_data = True
+    obj = render(request, 'index.html', data)
+    if 'error' in request.COOKIES:
+        obj.delete_cookie('error')
+    if success_data:
+        obj.delete_cookie('success_data')
+    return obj
+
+
+def process(request):
+    if request.method == 'GET':
+        return redirect('/')
+    if request.method == 'POST':
+        validation = input_validation(request.POST)
+        obj = redirect('/')
+        if validation != 'email' and validation != 'url' and validation != 'atc':
+            obj.set_cookie('success_data', 'successful')
+        else:
+            obj.set_cookie('error', validation)
+        return obj
 
 
 def error_403_view(request, reason=""):
@@ -16,16 +35,3 @@ def error_403_view(request, reason=""):
 
 def error_404_view(request, exception):
     return render(request, '404.html', status=404)
-
-
-def process(request):
-    try:
-        if request.method == 'GET':
-            return redirect('/')
-        if request.method == 'POST':
-            validation = input_validation(request.POST)
-            print(validation)
-            obj = redirect('/')
-            return obj
-    except UrlModel.DoesNotExist:
-        return HttpResponse('404')
