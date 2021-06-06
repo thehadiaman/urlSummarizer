6f3a92b32528baa04260873f8daf0ca20dc36853
HADI AMAN <hadiaman2.0@gmail.com>
2021-06-06 22:31:03 +0530

    UUID added

--- home/models.py
+++ home/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class UrlModel(models.Model):
     email = models.EmailField(500)
     original_url = models.URLField(500)
-    summarized_url = models.URLField(50)
+    summarized_url = models.UUIDField(unique=True)
