1151b6bbc6752070367b4bb7d226a2f685b123b1
HADI AMAN <hadiaman2.0@gmail.com>
2021-06-07 19:48:58 +0530

    added url dispatch for redirecting to target

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('process', process, name='process')
+    path('process', process, name='process'),
+    path('<str:url>', get_url, name='get_url')
 ]
