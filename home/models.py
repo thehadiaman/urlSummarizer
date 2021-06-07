d7a490a7f79530b0ed0ffcf003618c986b45d41e
HADI AMAN <hadiaman2.0@gmail.com>
2021-06-07 19:46:30 +0530

    Model is now capable of storing UUID

--- home/models.py
+++ home/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from uuid import uuid1
 
 
 class UrlModel(models.Model):
     email = models.EmailField(500)
     original_url = models.URLField(500)
-    summarized_url = models.UUIDField(unique=True)
+    summarized_url = models.UUIDField(default=uuid1, unique=True)
