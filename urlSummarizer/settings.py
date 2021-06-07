f810f69d55c97b0b2f89b25b398686f41e79212f
HADI AMAN <hadiaman2.0@gmail.com>
2021-06-07 20:21:22 +0530

    Settings changes for migration error and debugged

--- urlSummarizer/settings.py
+++ urlSummarizer/settings.py
@@ -49,7 +49,7 @@ WSGI_APPLICATION = 'urlSummarizer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
